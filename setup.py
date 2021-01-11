1606f640487669883d1add0c2a44802ad86a5a41
thismatters <paul@thismatters.net>
2021-01-11 10:59:08 -0600

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="django-svelte",
     author="Paul Stiverson",
     url="https://github.com/thismatters/django-svelte/",
-    version="0.1.1",
+    version="0.1.2",
     packages=["django_svelte"],
     license="MIT",
     description="Facilitates adding Svelte frontend to Django",
